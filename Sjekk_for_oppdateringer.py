d9fc667afbc5c3c634644b8a6afee40cd5c1c8a9
Ole Christian Hjertholm <147952918+Sirhiggelbottom@users.noreply.github.com>
2024-10-28 08:21:16 +0100

    |

--- Sjekk_for_oppdateringer.py
+++ Sjekk_for_oppdateringer.py
@@ -30,7 +30,7 @@ def check_updates():
 
             for line in upgradeable_files.stdout.splitlines():
                 if "upgraded," in line and "newly installed, " in line:
-                    upgraded_files = upgradeable_files.stdout.split('\n')[-2]
+                    upgraded_files = line
             
             send_telegram_message(f"Varsel fra {PI_NAME}:\n" + upgraded_files)
 
